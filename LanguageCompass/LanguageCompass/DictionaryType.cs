a0aeeb25c7afbee211231ff5952ce907284c174c
Phat Pham <155206042@GM.UIT.EDU.VN>
2017-09-07 20:28:09 +0700

    Add project files.

---
+++ LanguageCompass/LanguageCompass/DictionaryType.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using System;
+using System.Text.RegularExpressions;
+using System.Reflection;
+
+namespace LanguageCompass
+{
+    class DictionaryType: ContentPage
+    {
+        private static string dataDict;
+        public List<string> suggestKey = new List<string>();
+        public Dictionary<string, string> hashDict = new Dictionary<string, string>();
+
+
+        //Read Data
+        public void ReadData(string link)
+        {
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream(link);
+            try
+            {
+                using (StreamReader read = new StreamReader(stream))
+                {
+                    dataDict = read.ReadToEnd();
+                    dataDict = dataDict.Replace(@"\r\n\r\n", "~");
+                    DisplayAlert("cac", dataDict, "a");
+                }
+            }
+            catch (System.Exception e)
+            {
+                throw e;
+            }
+        }
+        //Set up Words for Dictionary type (hashDict)
+        public void SetUpForDictionary()
+        {
+            string[] fullWords = dataDict.Split(new string[] {  }, StringSplitOptions.RemoveEmptyEntries);
+            Regex regex = new Regex("(?<=@)[\\S\\D ]+?(?= /)");
+            foreach (string oneWord in fullWords)
+            {
+                try
+                {
+                    Match match;
+                    string name;
+                    match = regex.Match(oneWord);
+                    if (match.Success == false)
+                    {
+                        continue;
+                    }
+                    name = match.Value;
+                    suggestKey.Add(name);
+                    if (hashDict.ContainsKey(name) == false)
+                        hashDict.Add(name, oneWord);
+                }
+                catch { }
+            }
+            //Constructor
+            
+
+
+        }
+        public DictionaryType(string datalink)
+        {
+            ReadData(datalink);
+            SetUpForDictionary();
+        }
+
+
+
+    }
+}
---
+++ LanguageCompass/LanguageCompass/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms;
+using System.IO;
+
+
+namespace LanguageCompass
+{
+    public partial class MainPage : ContentPage
+    {
+        //latest searched word for history function
+        string lastSearchedWord = "";
+
+        //Datapath link
+        private static string historyPath;
+        
+
+        //language pair : en|vi, vi|en
+        private static string languagePair;
+        //Current using dictionary 
+        private static DictionaryType ev;
+        public MainPage()
+        {
+            InitializeComponent();
+            ev = new DictionaryType(@"LanguageCompass.EngtoVie.txt");
+        }
+
+        async private void Btn_search_Clicked(object sender, System.EventArgs e)
+        {
+            if (ev.hashDict.ContainsKey(entry_word.Text))
+                lb_content.Text = ev.hashDict[entry_word.Text];
+            else await DisplayAlert("Oops..", "404", "OK");
+        }
+    }
+
+}
